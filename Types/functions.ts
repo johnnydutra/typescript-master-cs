cdb89aafc8eaa04df071794e487ac5991a3322d5
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-27 12:57:39 -0300

    19: working with default parameters

--- Types/functions.ts
+++ Types/functions.ts
@@ -2,12 +2,13 @@ function square(num: number) {
   return num * num;
 }
 
-function greet(person: string) {
+function greet(person: string = 'stranger') {
   return `Hi there, ${person}!`;
 }
 
 const doSomething = (person: string, age: number, isFunny: boolean) => {};
 
 square(3);
+greet();
 greet('Tonya Harding');
 doSomething('ChickenFace', 78, false);
