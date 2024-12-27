ed591dfb0d9ba1263e278c755f0b8a7fc318ab06
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-27 16:56:57 -0300

    28: creating type aliases

--- Types/objects.ts
+++ Types/objects.ts
@@ -9,12 +9,33 @@ function printName(person: { first: string; last: string }) {
 }
 printName({ first: 'Thomas', last: 'Jenkins' });
 
-let coordinate: { x: number; y: number } = { x: 34, y: 2 };
+// let coordinate: { x: number; y: number } = { x: 34, y: 2 };
 
-function randomCoordinate(): { x: number; y: number } {
-  return { x: Math.random(), y: Math.random() };
-}
+// function randomCoordinate(): { x: number; y: number } {
+//   return { x: Math.random(), y: Math.random() };
+// }
 
 printName({ first: 'Mick', last: 'Jagger', age: 90 });
 const singer = { first: 'Mick', last: 'Jagger', age: 90 };
 printName(singer);
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+let coordinate: Point = { x: 34, y: 2 };
+
+function randomCoordinate(): Point {
+  return { x: Math.random(), y: Math.random() };
+}
+
+function doublePoint(point: Point): Point {
+  return {
+    x: point.x * 2,
+    y: point.y * 2,
+  };
+}
+
+type MyNum = number;
+let age: MyNum = 2323;
