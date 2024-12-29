929ccc3388d2c1b8c4e5e4b33d896d6aa260499c
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-29 16:54:42 -0300

    44: a bit more on tuples

--- Types/tuplesEnums.ts
+++ Types/tuplesEnums.ts
@@ -1,1 +1,7 @@
 const color: [number, number, number] = [255, 0, 255];
+
+type HttpResponse = [number, string];
+
+const goodRes: HttpResponse = [200, 'OK'];
+
+const responses: HttpResponse[] = [[404, 'Not Found'], goodRes];
