b4f55d01fbfb4b770a81ac8e88b8fd32b544cd25
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-29 08:19:41 -0300

    39: type narrowing with union types

--- Types/unionTypes.ts
+++ Types/unionTypes.ts
@@ -14,3 +14,18 @@ type Loc = {
 
 let coordinates: Point | Loc = { x: 1, y: 34 };
 coordinates = { lat: 321.213, lng: 23.334 };
+
+function printAge(age: number | string): void {
+  console.log(`You are ${age} years old`);
+}
+
+printAge(23);
+printAge('eighty seven');
+
+function calculateTax(price: number | string, tax: number) {
+  if (typeof price === 'string') {
+    price = parseFloat(price.replace('$', '')) * tax;
+  }
+
+  return price * tax;
+}
