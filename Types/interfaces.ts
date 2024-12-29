f78e9a329554844c7c0ba811c865fc6e5509cc97
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-29 17:21:26 -0300

    50 readonly and optional interface properties

--- Types/interfaces.ts
+++ Types/interfaces.ts
@@ -4,3 +4,16 @@ interface Point {
 }
 
 const pt: Point = { x: 123, y: 1234 };
+
+interface Person {
+  readonly id: number;
+  first: string;
+  last: string;
+  nickname?: string;
+}
+
+const thomas: Person = {
+  first: 'Thomas',
+  last: 'Hardy',
+  id: 21836,
+};
