1d28dea48ab30432cf9d1bf27a9780618f3920ff
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-29 17:03:58 -0300

    46: introducing enums

--- Types/tuplesEnums.ts
+++ Types/tuplesEnums.ts
@@ -5,3 +5,16 @@ type HttpResponse = [number, string];
 const goodRes: HttpResponse = [200, 'OK'];
 
 const responses: HttpResponse[] = [[404, 'Not Found'], goodRes];
+
+enum OrderStatus {
+  PENDING,
+  SHIPPED,
+  DELIVERED,
+  RETURNED,
+}
+
+const myStatus = OrderStatus.DELIVERED;
+
+function isDelivered(status: OrderStatus) {
+  return status === OrderStatus.DELIVERED;
+}
