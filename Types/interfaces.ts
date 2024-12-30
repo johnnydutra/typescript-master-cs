c3706318133548c3ea9f4740dd7f3583ceaa7ade
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-30 07:43:45 -0300

    53: reopening interfaces

--- Types/interfaces.ts
+++ Types/interfaces.ts
@@ -37,3 +37,22 @@ const shoes: Product = {
     return this.price;
   },
 };
+
+interface Dog {
+  name: string;
+  age: number;
+}
+
+interface Dog {
+  breed: string;
+  bark(): string;
+}
+
+const elton: Dog = {
+  name: 'Elton',
+  age: 0.5,
+  breed: 'Australian Shepherd',
+  bark() {
+    return 'WOOF WOOF!';
+  },
+};
